d29e43107c6b30d42ab34408d147ee44106a4723
damien <damien.seoul@gmail.com>
2021-08-03 10:40:54 +0800

    initial commit on 3rd Aug, 10:42 AM

---
+++ main.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var url = require('url');
+let topic = require('./lib/topic');
+let author = require('./lib/author');
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      topic.home(request, response);
+    } else {
+      topic.page(request, response);
+    }
+  } else if (pathname === '/create') {
+    topic.create(request, response);
+  } else if (pathname === '/create_process') {
+    topic.create_process(request, response);
+  } else if (pathname === '/update') {
+    topic.update(request, response);
+  } else if (pathname === '/update_process') {
+    topic.update_process(request, response);
+  } else if (pathname === '/delete_process') {
+    topic.delete_process(request, response);
+  } else if (pathname === '/author') {
+    author.list(request, response);
+  } else if (pathname === '/author_create') {
+    author.create(request, response);
+  } else if (pathname === '/author_update') {
+    author.update(request, response);
+  } else if (pathname === '/author_update_proc') {
+    author.author_update_proc(request, response);
+  } else if (pathname === '/author_delete') {
+    author.author_delete(request, response);
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+});
+app.listen(3000);
