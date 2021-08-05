e689b1ee1e44ef0ae1dd44e42236f1ce8b4275a1
damien <damien.seoul@gmail.com>
2021-08-05 18:24:37 +0800

    Conversion from if~else statement to Express framework

--- main.js
+++ main.js
@@ -1,42 +1,23 @@
-// for git pull testing
-var http = require('http');
-var url = require('url');
+// Updated from if .. else to Express
 let topic = require('./lib/topic');
 let author = require('./lib/author');
+const express = require('express')
+const app = express()
+const port = 3000;
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-      topic.home(request, response);
-    } else {
-      topic.page(request, response);
-    }
-  } else if (pathname === '/create') {
-    topic.create(request, response);
-  } else if (pathname === '/create_process') {
-    topic.create_process(request, response);
-  } else if (pathname === '/update') {
-    topic.update(request, response);
-  } else if (pathname === '/update_process') {
-    topic.update_process(request, response);
-  } else if (pathname === '/delete_process') {
-    topic.delete_process(request, response);
-  } else if (pathname === '/author') {
-    author.list(request, response);
-  } else if (pathname === '/author_create') {
-    author.create(request, response);
-  } else if (pathname === '/author_update') {
-    author.update(request, response);
-  } else if (pathname === '/author_update_proc') {
-    author.author_update_proc(request, response);
-  } else if (pathname === '/author_delete') {
-    author.author_delete(request, response);
-  } else {
-    response.writeHead(404);
-    response.end('Not found');
-  }
-});
-app.listen(3000);
+app.get('/', function(req, res) { topic.home(req, res); })
+app.get('/page/:pageId', function(req, res){ topic.page(req, res);})
+app.get('/create', function(req, res) { topic.create(req, res); })
+app.post('/create_process', function(req, res) { topic.create_process(req, res); })
+app.get('/update/:pageId', function(req, res) { topic.update(req, res); })
+app.post('/update_process', function(req, res) { topic.update_process(req, res); })
+app.post('/delete_process', function(req, res) { topic.delete_process(req, res); })
+app.get('/author', function(req, res) { author.list(req, res); })
+app.post('/author_create', function(req, res) { author.create(req, res); })
+app.get('/author_update/:authorId', function(req, res) { author.update(req, res); })
+app.post('/author_update_proc', function(req, res) { author.author_update_proc(req, res); })
+app.post('/author_delete', function(req, res) { author.author_delete(req, res);})
+
+app.listen(port, () => {
+  console.log(`Express() app listening at http://localhost:${port}`)
+})
